8412661b6fc1b5655523228074a0a0ac0d3ad1e3
rxn7 <rotthin_dev@protonmail.com>
2024-12-11 03:15:01 +0100

    Clean up cylinder code

--- src/shapes/cylinder.ts
+++ src/shapes/cylinder.ts
@@ -5,38 +5,44 @@ import { Vector3 } from '../vector3.js'
 export default class Cylinder extends Shape {
 	protected override vertices: Vector3[]
 	protected override centroid: Vector3 = new Vector3()
-	protected override connections: Connection[] = []
+	protected override connections: Connection[]
 
-	constructor(origin: Point, radius: number = 16, height: number = 256, segments: number = 12) {
+	constructor(origin: Point, private radius: number = 16, private height: number = 256, private segments: number = 12) {
 		super(origin)
-		this.vertices = new Array<Vector3>(segments * 2)
+		this.vertices = new Array<Vector3>(this.segments * 2)
+		this.connections = new Array<Connection>(this.segments * 3)
 
-		// Generate vertices
-		for(let i = 0; i < segments; ++i) {
-			const angle = (i / segments) * Math.PI * 2
-			const x = this.origin.x + Math.cos(angle) * radius
-			const y = this.origin.y + Math.sin(angle) * radius
+		this.generateVertices()
+		this.generateConnections()
+	}
 
-			// Bottom circle
+	private generateVertices(): void {
+		for(let i = 0; i < this.segments; ++i) {
+			const angle = (i / this.segments) * Math.PI * 2
+			const x = this.origin.x + Math.cos(angle) * this.radius
+			const y = this.origin.y + Math.sin(angle) * this.radius
+
+			// bottom circle
 			this.vertices[i] = new Vector3(x, y, 0)
 
-			// Top circle
-			this.vertices[i + segments] = new Vector3(x, y, height)
+			// top circle
+			this.vertices[i + this.segments] = new Vector3(x, y, this.height)
 		}
+	}
 
-		// Generate connections
-		for(let i = 0; i < segments; ++i) {
-			const next = (i + 1) % segments
+	private generateConnections(): void {
+		let idx = 0
+		for(let i = 0; i < this.segments; ++i) {
+			const next = (i + 1) % this.segments
 
-			// Connect bottom circle
-			this.connections.push({ a: i, b: next })
+			// connect to bottom circle
+			this.connections[idx++] = { a: i, b: next }
 
-			// Connect top circle
-			this.connections.push({ a: i + segments, b: next + segments })
+			// connect to top circle
+			this.connections[idx++] = { a: i + this.segments, b: next + this.segments }
 
-			// Connect vertical edges
-			this.connections.push({ a: i, b: i + segments })
+			// connect vertical edges
+			this.connections[idx++] = { a: i, b: i + this.segments }
 		}
 	}
 }
-
